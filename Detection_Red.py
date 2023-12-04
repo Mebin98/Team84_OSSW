d0db3f3dd6d336de59b0c0ea7313fe7f3b66cdac
mebin <mzmighty17@gmail.com>
2023-12-04 18:38:29 +0900

    Add red color detection functionality

---
+++ Detection_Red.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+
+def detect_red(image):
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    lower_red = np.array([0, 120, 70])
+    upper_red = np.array([10, 255, 255])
+    mask1 = cv2.inRange(hsv, lower_red, upper_red)
+
+    lower_red = np.array([170, 120, 70])
+    upper_red = np.array([180, 255, 255])
+    mask2 = cv2.inRange(hsv, lower_red, upper_red)
+
+    mask = mask1 + mask2
+    red_output = cv2.bitwise_and(image, image, mask=mask)
+    return red_output
+
+
